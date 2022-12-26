d77442e90d9d0f031906a26a51e49a3821dc3326
Marija Roncevic <101884288+marija12345678@users.noreply.github.com>
2022-12-26 18:49:48 +0100

    Update DataBase.cs

--- ServiceApp/DataBase.cs
+++ ServiceApp/DataBase.cs
@@ -6,16 +6,14 @@ using System.Text;
 
 namespace ServiceApp
 {
-    public class Database
-    {
+     public class Database
+     {
         internal static Dictionary<string, Korisnik> korisnici = new Dictionary<string, Korisnik>();
         internal static Dictionary<int, Koncert> koncerti = new Dictionary<int, Koncert>();
         internal static Dictionary<int, Rezervacija> rezervacije = new Dictionary<int, Rezervacija>();
 
         static Database()
         {
-
-
             Koncert k1 = new Koncert(1, "Opera", DateTime.Now.AddYears(8), "Novi Sad", 23);
             Koncert k2 = new Koncert(2, "Zeljko Joksimovic", DateTime.Now.AddYears(3), "Banja Luka", 50);
             Koncert k3 = new Koncert(3, "2000's", DateTime.Now.AddMonths(6), "Beograd", 10);
@@ -34,77 +32,66 @@ namespace ServiceApp
             rezervacije.Add(2, r1);
             rezervacije.Add(5, r2);
 
+
             Console.WriteLine("Spisak koncerata:");
             foreach (Koncert k in koncerti.Values)
             {
-                //Console.WriteLine($"\n\tKoncert: {k.Id}, {k.Naziv}, {k.VremePocetka}, {k.Lokacija}, {k.CenaKarte}");
-                string str = "";
-                str += $"\n\tKoncert: {k.Id}, {k.Naziv}, {k.VremePocetka}, {k.Lokacija}, {k.CenaKarte}";
-                //str += k.ToString();
-                Console.WriteLine(str);
+                k.ToString();
+                Console.WriteLine(k);
+
+                ////Console.WriteLine($"\n\tKoncert: {k.Id}, {k.Naziv}, {k.VremePocetka}, {k.Lokacija}, {k.CenaKarte}");
+                //string str = "";
+                //str += $"\n\tKoncert: {k.Id}, {k.Naziv}, {k.VremePocetka}, {k.Lokacija}, {k.CenaKarte}";
+                ////str += k.ToString();
+                //Console.WriteLine(str);
             }
 
             Console.WriteLine("Spisak rezervacija:");
             foreach (Rezervacija r in rezervacije.Values)
             {
-                string str = "";
-                str += $"\n\tRezervacije: {r.IdR}, {r.IdKoncerta}, {r.VremeRezervacije}, {r.StanjeRezervacije.ToString()}";
-                Console.WriteLine(str);
+                r.ToString();
+                Console.WriteLine(r);
+
+                //string str = "";
+                //str += $"\n\tRezervacije: {r.IdR}, {r.IdKoncerta}, {r.VremeRezervacije}, {r.StanjeRezervacije.ToString()}";
+                //Console.WriteLine(str);
             }
 
             //Console.WriteLine("Spisak korisnika:");
             //foreach (Korisnik k in korisnici.Values)
             //{
+            //    k.ToString();
+            //    ConsoleWriteLine(k);
+            //
+            //
+            //
             //    string str = "";
             //    str += $"\n\tKorisnici: {k.IdK}, {k.SredstvaNaRacunu}";
             //    Console.WriteLine(str);
             //}
 
             //Console.WriteLine($"\n\tKoncert: {k1.Id}, {k1.Naziv}, {k1.VremePocetka}, {k1.Lokacija}, {k1.CenaKarte}");
-
-            
-
-            //Console.WriteLine("Spisak koncerata:");
-            //foreach (Koncert k in koncerti.Values)
-            //{
-            //    koncerti.ToString();
-            //}
-
-            //Console.WriteLine("Spisak rezervacija:");
-            //foreach (Rezervacija r in rezervacije.Values)
-            //{
-            //    rezervacije.ToString();
-            //}
-
-            //Console.WriteLine("Spisak korisnika:");
-            //foreach (Korisnik k in korisnici.Values)
-            //{
-            //    korisnici.ToString();
-            //}
-
         }
 
-        //public override string ToString()
-        //{
-        //    Console.WriteLine("Spisak koncerata:");
-        //    foreach (Koncert k in koncerti.Values)
-        //    {
-        //        koncerti.ToString();
-        //    }
 
-        //    Console.WriteLine("Spisak rezervacija:");
-        //    foreach (Rezervacija r in rezervacije.Values)
-        //    {
-        //        rezervacije.ToString();
-        //    }
+        public override string ToString()
+        {
+            
+            Console.WriteLine("Spisak koncerata:");
+            foreach (Koncert k in koncerti.Values)
+            {
+                k.ToString();
+                Console.WriteLine(k);
+            }
 
-        //    Console.WriteLine("Spisak korisnika:");
-        //    foreach (Korisnik k in korisnici.Values)
-        //    {
-        //        korisnici.ToString();
-        //    }
+            Console.WriteLine("Spisak rezervacija:");
+            foreach (Rezervacija r in rezervacije.Values)
+            {
+                r.ToString();
+                Console.WriteLine(r);
 
-        //    return "";
-        //}
+            }
+            return "";
+        }
     }
 }
